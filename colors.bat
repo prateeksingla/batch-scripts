55916b13d0c072e140a00218f79da54c0871bca6
Prateek Singla <singla.prateek@gmail.com>
2013-06-13 14:37:22 +0530

    Create colors.bat
    Changes colours after one second, press space to make disco colors in DOS while executing this Batch file.

---
+++ colors.bat
@@ -0,0 +1,202 @@
+echo on
+cls
+color 00
+timeout /t 1
+color 10
+timeout /t 1
+color 20
+timeout /t 1
+color 30
+timeout /t 1
+color 40
+timeout /t 1
+color 50
+timeout /t 1
+color 60
+timeout /t 1
+color 70
+timeout /t 1
+color 80
+timeout /t 1
+color 90
+timeout /t 1
+color 01
+timeout /t 1
+color 11
+timeout /t 1
+color 21
+timeout /t 1
+color 31
+timeout /t 1
+color 41
+timeout /t 1
+color 51
+timeout /t 1
+color 61
+timeout /t 1
+color 71
+timeout /t 1
+color 81
+timeout /t 1
+color 91
+timeout /t 1
+color 02
+timeout /t 1
+color 12
+timeout /t 1
+color 22
+timeout /t 1
+color 32
+timeout /t 1
+color 42
+timeout /t 1
+color 52
+timeout /t 1
+color 62
+timeout /t 1
+color 72
+timeout /t 1
+color 82
+timeout /t 1
+color 92
+timeout /t 1
+color 03
+timeout /t 1
+color 13
+timeout /t 1
+color 23
+timeout /t 1
+color 33
+timeout /t 1
+color 43
+timeout /t 1
+color 53
+timeout /t 1
+color 63
+timeout /t 1
+color 73
+timeout /t 1
+color 83
+timeout /t 1
+color 93
+timeout /t 1
+color 04
+timeout /t 1
+color 14
+timeout /t 1
+color 24
+timeout /t 1
+color 34
+timeout /t 1
+color 44
+timeout /t 1
+color 54
+timeout /t 1
+color 64
+timeout /t 1
+color 74
+timeout /t 1
+color 84
+timeout /t 1
+color 94
+timeout /t 1
+color 05
+timeout /t 1
+color 15
+timeout /t 1
+color 25
+timeout /t 1
+color 35
+timeout /t 1
+color 45
+timeout /t 1
+color 55
+timeout /t 1
+color 65
+timeout /t 1
+color 75
+timeout /t 1
+color 85
+timeout /t 1
+color 95
+timeout /t 1
+color 06
+timeout /t 1
+color 16
+timeout /t 1
+color 26
+timeout /t 1
+color 36
+timeout /t 1
+color 46
+timeout /t 1
+color 56
+timeout /t 1
+color 66
+timeout /t 1
+color 76
+timeout /t 1
+color 86
+timeout /t 1
+color 96
+timeout /t 1
+color 07
+timeout /t 1
+color 17
+timeout /t 1
+color 27
+timeout /t 1
+color 37
+timeout /t 1
+color 47
+timeout /t 1
+color 57
+timeout /t 1
+color 67
+timeout /t 1
+color 77
+timeout /t 1
+color 87
+timeout /t 1
+color 97
+timeout /t 1
+color 08
+timeout /t 1
+color 18
+timeout /t 1
+color 28
+timeout /t 1
+color 38
+timeout /t 1
+color 48
+timeout /t 1
+color 58
+timeout /t 1
+color 68
+timeout /t 1
+color 78
+timeout /t 1
+color 88
+timeout /t 1
+color 98
+timeout /t 1
+color 09
+timeout /t 1
+color 19
+timeout /t 1
+color 29
+timeout /t 1
+color 39
+timeout /t 1
+color 49
+timeout /t 1
+color 59
+timeout /t 1
+color 69
+timeout /t 1
+color 79
+timeout /t 1
+color 89
+timeout /t 1
+color 99
+timeout /t 1
